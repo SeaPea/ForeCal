ecec2ef1200f59e1f136b4862c41b90434fb453f
SeaPea <SeaPea@users.noreply.github.com>
2014-04-24 21:51:29 -0700

    Added AM/PM indicator, Date, line

--- src/ForeCal.c
+++ src/ForeCal.c
@@ -2,11 +2,15 @@
 
 static Window *window;
 
+static Layer *current_layer = NULL;
 static TextLayer *clock_layer = NULL;
+static TextLayer *pm_layer = NULL;
+static TextLayer *date_layer = NULL;
+static AppTimer *bt_timer = NULL;
 static GBitmap *bt_icon = NULL;
-static BitmapLayer *bt_layer;
+static BitmapLayer *bt_layer = NULL;
 static GBitmap *batt_icon = NULL;
-static BitmapLayer *batt_layer;
+static BitmapLayer *batt_layer = NULL;
 //static TextLayer *temperature_layer;
 //static TextLayer *city_layer;
 //static BitmapLayer *icon_layer;
@@ -16,6 +20,7 @@ static BitmapLayer *batt_layer;
 //static uint8_t sync_buffer[64];
 
 static char current_time[6];
+static char current_date[] = "Sun Jan 10";
 
 /*enum WeatherKey {
   WEATHER_ICON_KEY = 0x0,         // TUPLE_INT
@@ -72,13 +77,32 @@ static void send_cmd(void) {
 }*/
 
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
-  clock_copy_time_string(current_time, 5);
+  clock_copy_time_string(current_time, 6);
   text_layer_set_text(clock_layer, current_time);
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Current time: %s", current_time);
 }
 
-static void handle_bt_update(bool connected) {
-  if (connected) {
+static void handle_hour_tick(struct tm *tick_time, TimeUnits units_changed) {
+  if (clock_is_24h_style()) {
+    text_layer_set_text(pm_layer, "");
+  }
+  else {
+    if (tick_time->tm_hour >= 12) {
+      text_layer_set_text(pm_layer, "PM");
+    }
+    else {
+      text_layer_set_text(pm_layer, "AM");
+    }
+  }
+}
+
+static void handle_day_tick(struct tm *tick_time, TimeUnits units_changed) {
+  strftime(current_date, sizeof(current_date), "%a %b %d", tick_time);
+  text_layer_set_text(date_layer, current_date);
+}
+
+static void update_bt_icon() {
+  if (bluetooth_connection_service_peek()) {
     APP_LOG(APP_LOG_LEVEL_DEBUG, "BT Connected");
     bitmap_layer_set_bitmap(bt_layer, bt_icon);
   }
@@ -86,6 +110,23 @@ static void handle_bt_update(bool connected) {
     APP_LOG(APP_LOG_LEVEL_DEBUG, "BT DISCONNECTED");
     bitmap_layer_set_bitmap(bt_layer, NULL);
     vibes_long_pulse();
+  }
+}
+
+static void handle_bt_timeout(void *data) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "BT Update - 15sec");
+  bt_timer = NULL;
+  update_bt_icon();
+}
+
+static void handle_bt_update(bool connected) {
+  if (bt_timer) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "BT Timer Rescheduled");
+    app_timer_reschedule(bt_timer, 15000);
+  }
+  else {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "BT Timer Scheduled");
+    bt_timer = app_timer_register(15000, handle_bt_timeout, NULL);
   }
 }
 
@@ -112,25 +153,60 @@ static void handle_batt_update(BatteryChargeState batt_status) {
   bitmap_layer_set_bitmap(batt_layer, batt_icon);
 }
 
+static void current_layer_draw(Layer *layer, GContext *ctx) {
+  graphics_context_set_stroke_color(ctx, GColorWhite);
+  graphics_draw_line(ctx, GPoint(0, 57), GPoint(144, 57));
+}
+
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   
-  clock_layer = text_layer_create(GRect(0, -12, 122, 50));
+  // Get current time
+  struct tm *t;
+  time_t temp;
+  temp = time(NULL);
+  t = localtime(&temp);
+  
+  current_layer = layer_create(GRect(0, 0, 144, 58));
+  layer_add_child(window_layer, current_layer);
+  
+  clock_layer = text_layer_create(GRect(-1, -14, 126, 50));
   text_layer_set_text_color(clock_layer, GColorWhite);
-  text_layer_set_background_color(clock_layer, GColorBlack);
+  text_layer_set_background_color(clock_layer, GColorClear);
   text_layer_set_font(clock_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
   text_layer_set_text_alignment(clock_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(clock_layer));
-  handle_minute_tick(NULL, MINUTE_UNIT);
+  text_layer_set_overflow_mode(clock_layer, GTextOverflowModeFill);
+  layer_add_child(current_layer, text_layer_get_layer(clock_layer));
+  handle_minute_tick(t, MINUTE_UNIT);
   
-  bt_layer = bitmap_layer_create(GRect(128, 1,9, 16));
-  layer_add_child(window_layer, bitmap_layer_get_layer(bt_layer));
-  handle_bt_update(bluetooth_connection_service_peek());
+  pm_layer = text_layer_create(GRect(123, 23, 20, 15));
+  text_layer_set_text_color(pm_layer, GColorWhite);
+  text_layer_set_background_color(pm_layer, GColorClear);
+  text_layer_set_font(pm_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  text_layer_set_text_alignment(pm_layer, GTextAlignmentCenter);
+  text_layer_set_overflow_mode(pm_layer, GTextOverflowModeFill);
+  layer_add_child(current_layer, text_layer_get_layer(pm_layer));
+  handle_hour_tick(t, HOUR_UNIT);
   
-  batt_layer = bitmap_layer_create(GRect(125, 20, 16, 8));
-  layer_add_child(window_layer, bitmap_layer_get_layer(batt_layer));
+  date_layer = text_layer_create(GRect(72, 30, 72, 26));
+  text_layer_set_text_color(date_layer, GColorWhite);
+  text_layer_set_background_color(date_layer, GColorClear);
+  text_layer_set_font(date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text_alignment(date_layer, GTextAlignmentRight);
+  text_layer_set_overflow_mode(date_layer, GTextOverflowModeFill);
+  layer_add_child(current_layer, text_layer_get_layer(date_layer));
+  handle_day_tick(t, DAY_UNIT);
+  
+  bt_layer = bitmap_layer_create(GRect(129, 1, 9, 16));
+  layer_add_child(current_layer, bitmap_layer_get_layer(bt_layer));
+  update_bt_icon();
+  
+  batt_layer = bitmap_layer_create(GRect(126, 18, 16, 8));
+  layer_add_child(current_layer, bitmap_layer_get_layer(batt_layer));
   BatteryChargeState batt_state = battery_state_service_peek();
   handle_batt_update(batt_state);
+  
+  layer_set_update_proc(current_layer, current_layer_draw);
   
   /*icon_layer = bitmap_layer_create(GRect(32, 10, 40, 40));
   layer_add_child(window_layer, bitmap_layer_get_layer(icon_layer));
@@ -188,6 +264,8 @@ static void init(void) {
   });
 
   tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+  tick_timer_service_subscribe(HOUR_UNIT, handle_hour_tick);
+  tick_timer_service_subscribe(DAY_UNIT, handle_day_tick);
   
   bt_icon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BT);
   bluetooth_connection_service_subscribe(handle_bt_update);
